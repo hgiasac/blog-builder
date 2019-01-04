bc28400813c585a75e7c21390cc148eb337eb94a
Toan Nguyen <hgiasac@gmail.com>
2019-01-04 18:42:44 +0700

    update head seo

--- src/builder/Template/Default.purs
+++ src/builder/Template/Default.purs
@@ -6,10 +6,12 @@ import Effect (Effect)
 import Template.Common (copyright, defaultNav, iconLink)
 import Text.Smolder.HTML as HTML
 import Text.Smolder.HTML.Attributes as HA
-import Text.Smolder.Markup (Markup, text, (!))
+import Text.Smolder.Markup (Attribute, Markup, attribute, text, (!))
 import Utils.String (stripeSentences)
 import Utils.Writer (assetsDir)
 
+property :: String -> Attribute
+property = attribute "property"
 
 defaultHead :: forall e. String -> Markup e 
 defaultHead title = HTML.head do 
@@ -17,6 +19,9 @@ defaultHead title = HTML.head do
   HTML.meta ! HA.name "viewport" ! HA.content "width=device-width, initial-scale=1"
 
   HTML.title $ text title 
+  HTML.meta ! property "og:title" ! HA.content title
+  HTML.meta ! property "twitter:title" ! HA.content title
+
   HTML.link ! HA.rel "stylesheet" ! HA.href "https://fonts.googleapis.com/css?family=Roboto:300,300i,500"
   HTML.link ! HA.rel "stylesheet" ! HA.href ("/" <> assetsDir <> "/main.min.css")
   HTML.link ! HA.rel "stylesheet" ! HA.href "https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css"
@@ -46,9 +51,14 @@ defaultTemplate title description content = do
   pure $ HTML.html $ do 
     defaultHead title
 
-    HTML.meta ! HA.name "description" ! HA.content (stripeSentences description 297)
+    HTML.meta ! HA.name "description" ! HA.content excerpt
+    HTML.meta ! property "og:description" ! HA.content excerpt
+    HTML.meta ! property "twitter:description" ! HA.content excerpt
     HTML.body do
       defaultHeader
       HTML.section ! HA.className "section" $ 
         HTML.div ! HA.id "main" $ content
       ftt
+
+  where
+    excerpt = stripeSentences description 297
